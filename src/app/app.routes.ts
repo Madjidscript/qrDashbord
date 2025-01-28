b7457ab9a28f051e3ca0d98f8e3c8e51c2d18417
Madjidscript <abdoul.latoundji@uvci.edu.ci>
2025-01-28 17:59:41 +0000

    depot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    
     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
     { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+    { path: '**', redirectTo: '/auth', pathMatch: 'full' }
 
 ];
